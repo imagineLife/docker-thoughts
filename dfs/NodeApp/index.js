cb6057592e4a2e69e81decccac774fd0e89c973f
Eric Laursen <ericjlaursen@gmail.com>
2020-07-12 18:46:17 -0400

    includes rm && publishing port

--- dfs/NodeApp/index.js
+++ dfs/NodeApp/index.js
@@ -27,6 +27,6 @@ console.log('server started!!');
 
 
 	RUNNING
-	docker run --init node-app
-	note: --init lets 'tini' allow command+c to close the service
+	docker run --init  --rm --publish 3000:3000 node-app
+	notes: --init lets 'tini' allow command+c to close the service
 */
