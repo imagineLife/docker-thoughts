784992ac5f8a31eec86d8ad56e3fa568938de4ad
Eric Laursen <ericjlaursen@gmail.com>
2020-07-14 21:05:13 -0400

    init

---
+++ dfs/NodeAlipneFromScratch/index.js
@@ -0,0 +1,52 @@
+const hapi = require("@hapi/hapi");
+
+//server config
+const serverObj = {
+	// cannot use localhost here
+	// breaking infinite call loop if-so
+  host: "0.0.0.0",
+  port: process.env.PORT || 3321
+}
+
+const rootPathHandlerObj = {
+  method: "GET",
+  path: "/",
+  handler() {
+    return { success: true };
+  }
+}
+
+// for registering hapi-pino
+const hapiPinoObj = {
+  plugin: require("hapi-pino"),
+  options: {
+    prettyPrint: true
+  }
+}
+
+async function start() {
+  const server = hapi.server(serverObj);
+
+  server.route(rootPathHandlerObj);
+
+  await server.register(hapiPinoObj);
+
+  await server.start();
+
+  return server;
+}
+
+start().catch(err => {
+  console.log(err);
+  process.exit(1);
+});
+
+
+/*
+
+TRY inspecting node-alpine
+docker inspect alpine;3.10 | jq
+
+
+
+*/
