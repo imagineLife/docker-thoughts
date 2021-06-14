9ea6c94c88977021fe6c95e259c7ff31b1a27b0a
Eric Laursen <ericjlaursen@gmail.com>
2021-06-14 07:01:25 -0400

    dep files

--- containers/7-express-server/index.js
+++ containers/7-express-server/index.js
@@ -1,8 +1,10 @@
 const e = require('express');
 const app = e();
+const PORT = process.env.port || 3000;
 
 function rootHandler(req,res){
   res.end('test');
 }
 
 app.get('/',rootHandler);
+app.listen(PORT, () => console.log(`Express server listening on port ${PORT}`))
