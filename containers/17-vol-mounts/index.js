c8c5fb91b256600b722eac2e51055640f560cf70
Eric Laursen <ericjlaursen@gmail.com>
2021-08-07 15:47:37 -0400

    remove logs

--- containers/17-vol-mounts/index.js
+++ containers/17-vol-mounts/index.js
@@ -6,28 +6,18 @@ const dataPath = join(process.env.DATA_PATH || "./data.txt");
 function writeTo(d){
   writeFile(dataPath, d.toString(), (err, res) => {
     if(err){
-      console.log('WRITE err')
-      console.log(err)
       return;
     }else{
-      console.log('write successful!')
+      console.log(`write ${d} successful!`)
     }
   })
 }
 
 readFile(dataPath, (err, bufferRes) => {
   if(err){
-    console.log('readfile err')
-    console.log(err)
-    console.error(err);
     writeTo(0)
     return;
   }
-
   const strRes = bufferRes.toString();
-  console.log(`STR RES`)
-  console.log(strRes);
-  console.log('// - - - - - //')
-  
   writeTo(+strRes + 1);
 })
