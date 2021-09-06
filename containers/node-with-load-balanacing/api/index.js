79e25d29df3cf8acf3ec26f4ce0c96b1be76fb99
Eric Laursen <ericjlaursen@gmail.com>
2021-09-06 08:20:27 -0400

    init simple example http server

---
+++ containers/node-with-load-balanacing/api/index.js
@@ -0,0 +1,10 @@
+const httpServer = require('express')()
+const apiID = process.env.API_ID;
+
+// Register endpoint listeners
+httpServer.get('/', (req,res) => res.send(`API ID ${apiID} says hey`));
+httpServer.get('/api1', (req,res) => res.send(`API ID ${apiID} api1`));
+httpServer.get('/api2', (req,res) => res.send(`API ID ${apiID} api2`));
+httpServer.get('/admin', (req,res) => res.send(`API ID ${apiID} secret admin api`));
+
+httpServer.listen(9999, () => console.log(`http server id ${apiID} listening on 9999`))
